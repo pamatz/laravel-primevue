7ce3e1649d9fd7181dfc32c176e2ed7fa6d92862
Roman Pamatz <rdpamatz@gmail.com>
2025-12-17 03:01:20 -0600

    Se quita shadcn-vue

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,7 +4,6 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
-import { initializeTheme } from './composables/useAppearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -20,10 +19,4 @@ createInertiaApp({
             .use(plugin)
             .mount(el);
     },
-    progress: {
-        color: '#4B5563',
-    },
 });
-
-// This will set light / dark mode on page load...
-initializeTheme();
